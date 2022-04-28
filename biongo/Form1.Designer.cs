c7679e78f22ec75b46eb6c74818ea7c68c0b1597
viss5vuus98 <dokipu@gmail.com>
2022-04-28 23:48:43 +0800

    commit-UI

---
+++ biongo/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace biongo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserInput = new System.Windows.Forms.Label();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum0 = new System.Windows.Forms.Button();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.lblUserNum = new System.Windows.Forms.Label();
+            this.lblRendamNum = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUserInput
+            // 
+            this.lblUserInput.BackColor = System.Drawing.Color.White;
+            this.lblUserInput.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblUserInput.Location = new System.Drawing.Point(419, 241);
+            this.lblUserInput.Name = "lblUserInput";
+            this.lblUserInput.Size = new System.Drawing.Size(112, 50);
+            this.lblUserInput.TabIndex = 0;
+            this.lblUserInput.Text = "輸入號碼";
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum1.Location = new System.Drawing.Point(112, 126);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(55, 52);
+            this.btnNum1.TabIndex = 1;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.btnNum1_Click);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum2.Location = new System.Drawing.Point(190, 126);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(55, 52);
+            this.btnNum2.TabIndex = 1;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.btnNum2_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum3.Location = new System.Drawing.Point(265, 126);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(55, 52);
+            this.btnNum3.TabIndex = 1;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.btnNum3_Click);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum4.Location = new System.Drawing.Point(341, 126);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(55, 52);
+            this.btnNum4.TabIndex = 1;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.btnNum4_Click);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum5.Location = new System.Drawing.Point(423, 126);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(55, 52);
+            this.btnNum5.TabIndex = 1;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.btnNum5_Click);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum6.Location = new System.Drawing.Point(505, 126);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(55, 52);
+            this.btnNum6.TabIndex = 1;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.btnNum6_Click);
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum7.Location = new System.Drawing.Point(584, 126);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(55, 52);
+            this.btnNum7.TabIndex = 1;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.btnNum7_Click);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum8.Location = new System.Drawing.Point(666, 126);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(55, 52);
+            this.btnNum8.TabIndex = 1;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.btnNum8_Click);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum9.Location = new System.Drawing.Point(743, 126);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(55, 52);
+            this.btnNum9.TabIndex = 1;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.btnNum9_Click);
+            // 
+            // btnNum0
+            // 
+            this.btnNum0.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNum0.Location = new System.Drawing.Point(818, 126);
+            this.btnNum0.Name = "btnNum0";
+            this.btnNum0.Size = new System.Drawing.Size(55, 52);
+            this.btnNum0.TabIndex = 1;
+            this.btnNum0.Text = "0";
+            this.btnNum0.UseVisualStyleBackColor = true;
+            this.btnNum0.Click += new System.EventHandler(this.btnNum0_Click);
+            // 
+            // btnInput
+            // 
+            this.btnInput.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnInput.Location = new System.Drawing.Point(569, 241);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(82, 50);
+            this.btnInput.TabIndex = 2;
+            this.btnInput.Text = "輸入";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // lblUserNum
+            // 
+            this.lblUserNum.BackColor = System.Drawing.Color.Snow;
+            this.lblUserNum.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblUserNum.Location = new System.Drawing.Point(112, 397);
+            this.lblUserNum.Name = "lblUserNum";
+            this.lblUserNum.Size = new System.Drawing.Size(808, 63);
+            this.lblUserNum.TabIndex = 3;
+            // 
+            // lblRendamNum
+            // 
+            this.lblRendamNum.BackColor = System.Drawing.Color.Snow;
+            this.lblRendamNum.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblRendamNum.Location = new System.Drawing.Point(112, 619);
+            this.lblRendamNum.Name = "lblRendamNum";
+            this.lblRendamNum.Size = new System.Drawing.Size(808, 63);
+            this.lblRendamNum.TabIndex = 3;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BackColor = System.Drawing.Color.Snow;
+            this.lblAnswer.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblAnswer.Location = new System.Drawing.Point(112, 857);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(808, 63);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "兌獎結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(985, 1008);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblRendamNum);
+            this.Controls.Add(this.lblUserNum);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.btnNum0);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.lblUserInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserInput;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Label lblUserNum;
+        private System.Windows.Forms.Label lblRendamNum;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ biongo/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace biongo
+{
+    public partial class Form1 : Form
+    {
+        string inputNum;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            inputNum += "1";
+            
+            lblUserInput.Text = inputNum;
+            
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            inputNum += "2";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            inputNum += "3";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            inputNum += "4";
+            
+            lblUserInput.Text = inputNum;
+
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            inputNum += "5";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            inputNum += "6";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            inputNum += "7";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            inputNum += "8";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum9_Click(object sender, EventArgs e)
+        {
+            inputNum += "9";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e)
+        {
+            inputNum += "0";
+            
+            lblUserInput.Text = inputNum;
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(inputNum) > 80 || Convert.ToInt32(inputNum) == 0) //限制輸入 1~40
+            {
+                inputNum = "";
+                lblUserInput.Text = "";
+                MessageBox.Show("數字輸入錯誤");
+                return;
+            }
+            lblUserNum.Text += $"{inputNum},";
+            inputNum = "";
+            lblUserInput.Text = "";
+            count++;
+            if (count >= 10)
+            {
+                getRandomNum();
+                string all = lblRendamNum.Text;
+                string[] subs = all.Split(',',' ');
+                foreach (var item in subs)
+                {
+                    Console.WriteLine($"Substring: {item}");
+                }
+            }
+
+            //輸入10個後呼叫函式開獎 用sliot切開字串
+
+        }
+        void getRandomNum()
+        {
+            Random 亂數 = new Random();//亂數種子
+
+            for (int i = 0; i < 10; i++)
+            {
+                int index = 亂數.Next(1, 81);//回傳1-80的亂數
+                lblRendamNum.Text += $"{index},";
+            }
+
+        }
+
+        
+    }
+}
